dda54fad6800bcbb393ed51fb3af37b61a953755
dazeddd <joonsoolee14@gmail.com>
2023-11-25 17:37:05 +0900

    initial commit

---
+++ session.py
@@ -0,0 +1,12 @@
+# https://spark.apache.org/docs/latest/sql-getting-started.html
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+
+
